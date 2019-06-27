94c1c24e159460b27a660efeafcf5e33b9de63b3
Edison <Edison@DESKTOP-8AJQAJJ>
2019-06-26 19:11:57 -0500

    Inicio del trabajo Colaborativo

---
+++ src/ingswoct2019/IngSWoct2019.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingswoct2019;
+
+/**
+ *
+ * @author 
+ */
+public class IngSWoct2019 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
